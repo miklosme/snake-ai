04dd35d49e2279484a481261cc4627a1a3360423
Miklos Megyes <m.miklos05@gmail.com>
2023-07-10 13:29:54 +0200

    save model

--- agent.py
+++ agent.py
@@ -130,7 +130,7 @@ def train():
 
             if score > record:
                 record = score
-                # agent.model.save()
+                agent.model.save()
 
             print("Game", agent.n_games, "Score", score, "Record", record)
 
